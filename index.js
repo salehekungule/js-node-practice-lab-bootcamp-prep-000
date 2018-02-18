507f6c09af217ba65f17bc006d456e1c46699374
Salehe Kungule <s.kungule@irdp.ac.tz>
2018-02-18 11:49:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Salehe"
-var height = "91"
+var height = 91
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(height+1)
